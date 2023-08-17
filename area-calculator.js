0e68e0610c6ea360931f5feb6772babcbd176763
Alfred Gomes <alfredgomes23@users.noreply.github.com>
2023-08-17 23:09:35 +0600

    rhombus and pentagon card is done.

--- area-calculator.js
+++ area-calculator.js
@@ -16,14 +16,31 @@ function getValueById(id){
 
 function calculateTriangleArea(){
     const base = getValueById('tri-base');
-    const hight = getValueById('tri-hight');
-    if(base == 0 || hight == 0) return;
-    document.getElementById('tri-area').innerText = (0.5*base*hight);
+    const height = getValueById('tri-height');
+    if(base == 0 || height == 0) return;
+    document.getElementById('tri-area').innerText = (0.5*base*height);
 }
 function calculateRectangleArea(){
     const width = getValueById('rec-width');
     const length = getValueById('rec-length');
     if (width == 0 || length == 0) return;
     document.getElementById('rec-area').innerText = (width*length);
-
+}
+function calculateParallelogramArea(){
+    const base = getValueById('para-base');
+    const height = getValueById('para-height');
+    if (base == 0 || height == 0) return;
+    document.getElementById('para-area').innerText = (base*height);
+}
+function calculateRhombusArea(){
+    const d1 = getValueById('d1');
+    const d2 = getValueById('d2');
+    if (d1 == 0 || d2 == 0) return;
+    document.getElementById('rhombus-area').innerText = (0.5 * d1 * d2);
+}
+function calculatePentagonArea(){
+    const peri = getValueById('penta-peri');
+    const base = getValueById('penta-base');
+    if (peri == 0 || base == 0) return;
+    document.getElementById('penta-area').innerText = (0.5 * peri * base);
 }
